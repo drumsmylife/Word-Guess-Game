2606f3ec6cddf3eaff0d371c2168bef4373336fa
Tommy Cervenka <drumsmylife@Tommys-MacBook-Pro.local>
2018-12-12 21:56:05 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,9 @@ var words = ["hockey", "fish", "drummer", "spaghetti", "monopoly", "airplane", "
 // Choose word randomly
 var randNum = Math.floor(Math.random() * words.length);
 var currentWord = words[randNum];
-var correctLetter = [];
 var lettersGuessed = [];
 var underScore = [];
+var wins = 0;
 
 var guessesLeft = 10;
 
@@ -43,10 +43,17 @@ document.addEventListener("keypress", (event) => {
 //if users guess is right 
     if(currentWord.indexOf(keyPress) > -1){
         //replace underscore with correct letter
-        underScore[currentWord.indexOf(keyPress)] = keyPress;
+        for(var i=currentWord.indexOf(keyPress); i > -1; i=currentWord.indexOf(keyPress,i+1)) {
+            underScore[i] = keyPress;  
+        }
+            
+            
+      
         //checks to see if user word matches guess
-        if(underScore === currentWord) {
+        if(arrayEqualToString(underScore,currentWord)) {
             alert("you win")
+            wins = wins + 1;
+            startNewGame ()
         } else {
            guessesLeft = guessesLeft - 1;
         }
@@ -56,11 +63,39 @@ document.addEventListener("keypress", (event) => {
     } 
     if (guessesLeft === 0) {
         alert("You Lose");
+        startNewGame ()
     }
     //update page
     document.getElementById("guesses").innerHTML = guessesLeft;
     document.getElementById("wrong_guesses").innerHTML = lettersGuessed;
     document.getElementById("right_guesses").innerHTML = formatArray(underScore);
+    document.getElementById("wins").innerHTML = wins;
 });
 
+function onLoadFunction() {
+    document.getElementById("wins").innerHTML = wins;
+}    
 
+function arrayEqualToString(underScore,currentWord) {
+    if (underScore.length !== currentWord.length) {
+        return false;
+
+    }
+    for(var i = 0; i < currentWord.length; i++) {
+       if (currentWord [i] !== underScore [i]) {
+        return false;
+       }
+    } 
+    return true; 
+}
+
+function startNewGame() {
+    randNum = Math.floor(Math.random() * words.length);
+    currentWord = words[randNum];
+    guessesLeft = 10;
+    lettersGuessed = [];
+    underScore = [];
+    generateUnderscore ()
+
+
+}
